6a9fff3b5bd9060afddb7f051290a72326428635
PurpleMyst <PurpleMyst@users.noreply.github.com>
2026-07-09 14:11:00 +0200

    perf(puzzle03): exploit fixed alphabet

--- puzzle03/src/lib.rs
+++ puzzle03/src/lib.rs
@@ -1,7 +1,5 @@
 use std::fmt::Display;
 
-use rayon::prelude::*;
-
 #[inline]
 pub fn solve() -> (impl Display, impl Display, impl Display) {
     (solve_part1(), solve_part2(), solve_part3())
@@ -12,10 +10,9 @@ pub fn solve_part1() -> impl Display {
     include_str!("input.txt")
         .lines()
         .max_by_key(|line| {
-            line.len() as u16
-                * (line.bytes().any(|c| c.is_ascii_lowercase()) as u16
-                    + line.bytes().any(|c| c.is_ascii_uppercase()) as u16
-                    + line.bytes().any(|c| c.is_ascii_digit()) as u16)
+            let flags = line.bytes().fold(0, |flags, c| flags | BYTE_FLAGS[c as usize]);
+
+            line.len() as u16 * (flags & CLASS_MASK).count_ones() as u16
         })
         .unwrap()
 }
@@ -30,14 +27,14 @@ pub fn solve_part2() -> impl Display {
 
 #[inline]
 pub fn solve_part3() -> impl Display {
-    let states = include_str!("input.txt")
-        .par_lines()
-        .map(|line| State::default().push_many(line.bytes()))
-        .collect::<Vec<_>>();
+    let mut states = Vec::with_capacity(100);
+    for line in include_str!("input.txt").lines() {
+        states.push(State::default().push_many(line.bytes()));
+    }
 
     b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        .into_par_iter()
-        .map(|&big_c| states.iter().map(|state| state.push(big_c).value()).sum::<u16>())
+        .iter()
+        .map(|&big_c| states.iter().map(|state| state.value_after_push(big_c)).sum::<u16>())
         .max()
         .unwrap()
 }
@@ -58,6 +55,44 @@ const SAW_DIGIT: u8 = 1 << 2;
 const SAW_SEVEN: u8 = 1 << 3;
 const SAW_NONSEVEN: u8 = 1 << 4;
 const CLASS_MASK: u8 = SAW_LOWER | SAW_UPPER | SAW_DIGIT;
+const FLAG_MASK: u8 = CLASS_MASK | SAW_SEVEN | SAW_NONSEVEN;
+const BYTE_FLAGS: [u8; 256] = {
+    let mut flags = [0; 256];
+    let mut c = b'0';
+    while c <= b'9' {
+        flags[c as usize] = SAW_DIGIT | SAW_NONSEVEN;
+        c += 1;
+    }
+    flags[b'7' as usize] = SAW_DIGIT | SAW_SEVEN;
+
+    c = b'A';
+    while c <= b'Z' {
+        flags[c as usize] = SAW_UPPER;
+        c += 1;
+    }
+
+    c = b'a';
+    while c <= b'z' {
+        flags[c as usize] = SAW_LOWER;
+        c += 1;
+    }
+
+    flags
+};
+const FLAG_SCORE: [u16; 32] = {
+    let mut scores = [0; 32];
+    let mut flags = 0;
+    while flags < 32 {
+        let flags_u8 = flags as u8;
+        scores[flags] = (flags_u8 & SAW_LOWER != 0) as u16
+            + (flags_u8 & SAW_UPPER != 0) as u16
+            + (flags_u8 & SAW_DIGIT != 0) as u16
+            + 7 * ((flags_u8 & SAW_SEVEN != 0 && flags_u8 & SAW_NONSEVEN == 0) as u16);
+        flags += 1;
+    }
+
+    scores
+};
 
 #[derive(Default, Clone, Copy)]
 enum ColorState {
@@ -80,34 +115,9 @@ enum ColorState {
     Blue,
 }
 
-impl State {
+impl ColorState {
     fn push(self, c: u8) -> Self {
-        let mut new = self;
-        if c.is_ascii_lowercase() {
-            new.flags |= SAW_LOWER;
-        } else if c.is_ascii_uppercase() {
-            new.flags |= SAW_UPPER;
-        } else if c.is_ascii_digit() {
-            new.flags |= SAW_DIGIT;
-
-            if c == b'7' {
-                new.flags |= SAW_SEVEN;
-            } else {
-                new.flags |= SAW_NONSEVEN;
-            }
-        }
-
-        if c == self.run_char {
-            new.run_length += 1;
-            if new.run_length >= 3 {
-                new.best_run_length = new.best_run_length.max(new.run_length);
-            }
-        } else {
-            new.run_char = c;
-            new.run_length = 1;
-        }
-
-        new.color_state = match (self.color_state, c) {
+        match (self, c) {
             (ColorState::Idle, b'g') => ColorState::G,
 
             (ColorState::G, b'r') => ColorState::Gr,
@@ -126,10 +136,37 @@ impl State {
             (ColorState::Bl, b'u') => ColorState::Blu,
             (ColorState::Blu, b'e') => ColorState::Blue,
 
-            (ColorState::Green, _) | (ColorState::Red, _) | (ColorState::Blue, _) => self.color_state,
+            (ColorState::Green, _) | (ColorState::Red, _) | (ColorState::Blue, _) => self,
 
             _ => ColorState::Idle,
-        };
+        }
+    }
+
+    fn multiplier(self) -> u16 {
+        if matches!(self, ColorState::Green | ColorState::Red | ColorState::Blue) {
+            3
+        } else {
+            1
+        }
+    }
+}
+
+impl State {
+    fn push(self, c: u8) -> Self {
+        let mut new = self;
+        new.flags |= BYTE_FLAGS[c as usize];
+
+        if c == self.run_char {
+            new.run_length += 1;
+            if new.run_length >= 3 {
+                new.best_run_length = new.best_run_length.max(new.run_length);
+            }
+        } else {
+            new.run_char = c;
+            new.run_length = 1;
+        }
+
+        new.color_state = self.color_state.push(c);
 
         new.len += 1;
 
@@ -140,15 +177,29 @@ impl State {
         s.into_iter().fold(self, |state, c| state.push(c))
     }
 
+    fn value_after_push(self, c: u8) -> u16 {
+        let flags = self.flags | BYTE_FLAGS[c as usize];
+        let best_run_length = if c == self.run_char {
+            let run_length = self.run_length + 1;
+            if run_length >= 3 {
+                self.best_run_length.max(run_length)
+            } else {
+                self.best_run_length
+            }
+        } else {
+            self.best_run_length
+        };
+
+        (self.len as u16 + 1)
+            * (FLAG_SCORE[(flags & FLAG_MASK) as usize] + best_run_length as u16 * best_run_length as u16)
+            * self.color_state.push(c).multiplier()
+    }
+
     fn value(self) -> u16 {
         self.len as u16
             * ((self.flags & CLASS_MASK).count_ones() as u16
                 + 7 * ((self.flags & SAW_SEVEN != 0 && self.flags & SAW_NONSEVEN == 0) as u16)
                 + self.best_run_length as u16 * self.best_run_length as u16)
-            * (if matches!(self.color_state, ColorState::Green | ColorState::Red | ColorState::Blue) {
-                3
-            } else {
-                1
-            })
+            * self.color_state.multiplier()
     }
 }
